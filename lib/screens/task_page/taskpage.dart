07ed63c15f5646b375f1c469c3d0b441139afb1d
visal <visalrajan@gmail.com>
2021-12-23 13:01:25 +0530

    new changes in punch attendance and firestore settings

--- lib/screens/task_page/taskpage.dart
+++ lib/screens/task_page/taskpage.dart
@@ -23,49 +23,7 @@ class _TaskPageState extends State<TaskPage> {
         ),
         centerTitle: true,
       ),
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //       child: Center(
-      //         child: Container(
-      //           height: 50,
-      //           width: 250,
-      //           child:  ElevatedButton(
-      //             style: ButtonStyle(
-      //               shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-      //                 RoundedRectangleBorder(
-      //                   borderRadius: BorderRadius.circular(18.0),
-      //                   side: BorderSide(
-      //                     color: Colors.teal,
-      //                     width: 2.0,
-      //                   ),
-      //                 ),
-      //               ),
-      //             ),
-      //             child: Text('EMPLOYEE DETAILS',
-      //               style: TextStyle(
-      //                 fontSize: 24,
-      //                 fontWeight: FontWeight.bold,
-      //               ),),
-      //             onPressed: () {
-      //               Navigator.of(context).pushReplacement(MaterialPageRoute(
-      //                   builder: (context) => EmployeeDetails()));
-      //             },
-      //           ),
-      //         ),
-      //
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 50,
-      //     ),
-      //
-      //
-      //   ],
-      //
-      // ),
+
     );
   }
 }
